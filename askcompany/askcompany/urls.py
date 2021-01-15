978e8c0e3dbe8cb62d5a2a7ff767a88cc8375b72
siyeon Kim <siyeonk27@gmail.com>
2021-01-16 03:37:07 +0900

    5강

--- askcompany/askcompany/urls.py
+++ askcompany/askcompany/urls.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
-
-def mysum(request, x, y):
-    result = x+y+100
-    return HttpResponse('result = {}'.format(result))
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('mysum/<int:x>/<int:y>', mysum),
+    path('shop/', include('shop.urls')),
 ]

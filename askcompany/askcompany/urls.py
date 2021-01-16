41b4d9bad23528a0af18ad1164a380dd5e7a12a1
siyeon Kim <siyeonk27@gmail.com>
2021-01-16 23:22:46 +0900

    16강

--- askcompany/askcompany/urls.py
+++ askcompany/askcompany/urls.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from django.conf import settings
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shop.urls')),
     path('blog/', include('blog.urls')),
 ]
+
+
+urlpatterns += [
+
+    path('__debug__/', include(debug_toolbar.urls)),
+]   

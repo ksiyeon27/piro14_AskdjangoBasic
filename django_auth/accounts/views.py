222fe7bb17fc815ac9b0d2a2c239f12bb928cb09
siyeon Kim <siyeonk27@gmail.com>
2021-01-19 04:19:06 +0900

    중급편 7강

--- django_auth/accounts/views.py
+++ django_auth/accounts/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, resolve_url, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.conf import settings
 from django.views.generic import CreateView
 from django.contrib.auth.models import User
 from .forms import SignupForm
+from django.contrib.auth import login as auth_login
 
 
 @login_required
@@ -17,7 +18,9 @@ def profile(request):
 #         form = SignupForm(request.POST)
 #         if form.is_valid():
 #             user = form.save()
-#             return redirect(settings.LOGIN_URL)
+#             #  로그인 처리 여기서 해주면 됨
+#             auth_login(request, user)
+#             return redirect("profile")
 #     else:
 #         form = SignupForm()
 #     return render(
@@ -28,10 +31,28 @@ def profile(request):
 #         },
 #     )
 
+##first CBV
+# signup = CreateView.as_view(
+#     model=User,
+#     form_class=SignupForm,
+#     template_name="accounts/signup.html",
+#     success_url=settings.LOGIN_URL,
+# )
 
-signup = CreateView.as_view(
-    model=User,
-    form_class=SignupForm,
-    template_name="accounts/signup.html",
-    success_url=settings.LOGIN_URL,
-)
+
+##new CBV
+class SignupView(CreateView):
+    model = User
+    form_class = SignupForm
+    template_name = "accounts/signup.html"
+
+    def get_success_url(self):
+        return resolve_url("profile")
+
+    def form_valid(self, form):
+        user = form.save()
+        auth_login(self.request, user)
+        return redirect(self.get_success_url())
+
+
+signup = SignupView.as_view()

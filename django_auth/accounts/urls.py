95a7474c0324720e24be956e1f60905e91c9a7d3
siyeon Kim <siyeonk27@gmail.com>
2021-01-19 02:20:50 +0900

    중급편 4강

--- django_auth/accounts/urls.py
+++ django_auth/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+
 # accounts는 앱네임 지정 안한대
 
 urlpatterns = [
@@ -9,6 +10,6 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name="accounts/login.html"),
         name="login",
     ),  # 설정 재정의
-    path('profile/', views.profile, name = 'profile'),
-    
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("profile/", views.profile, name="profile"),
 ]
